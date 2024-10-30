20a690643c1e558ffb0aeac3a75270dadf8770ad
Dimitris Stavroulakis <jimstav@gmail.com>
2024-10-30 12:53:38 +0200

    CardList Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
-
 import "./App.css";
+import CardList from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -54,13 +54,14 @@ class App extends Component {
           placeholder="Search monsters"
           onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+        <CardList />
       </div>
     );
   }
